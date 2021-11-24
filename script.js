24a68a32d30e56a70e92d29138475a2c3fa7e8b8
Jan Dreier <Dreier-96@web.de>
2021-11-24 16:37:36 +0100

    basic game functional

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
 let fields = [];
 
-function fillShape(id) {
-    fields[id] = '';
+let currentShape = ['cross'];
 
+function fillShape(id) {
+    if (!fields[id]) {
+
+
+        if (currentShape == 'cross') {
+            currentShape = 'circle';
+            document.getElementById('player-1').classList.add('player-inactive');
+            document.getElementById('player-2').classList.remove('player-inactive');
+
+        } else {
+            currentShape = 'cross';
+            document.getElementById('player-1').classList.remove('player-inactive');
+            document.getElementById('player-2').classList.add('player-inactive');
+        }
+
+        fields[id] = currentShape;
+        draw();
+        checkForWin();
+    }
+}
+
+function draw() {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
+            document.getElementById('circle-' + i).classList.remove('d-none')
+        }
+
+        if (fields[i] == 'cross') {
+            document.getElementById('cross-' + i).classList.remove('d-none')
+        }
+
+    }
+}
+
+function checkForWin() {
+    let winner;
+
+    if (fields[0] == fields[1] && fields[1] == fields[2]) {
+        winner = fields[0];
+    } else if (fields[3] == fields[4] && fields[4] == fields[5]) {
+        winner = fields[3];
+    } else if (fields[6] == fields[7] && fields[7] == fields[8]) {
+        winner = fields[6];
+    } else if (fields[0] == fields[3] && fields[3] == fields[6]) {
+        winner = fields[0];
+    } else if (fields[1] == fields[4] && fields[4] == fields[7]) {
+        winner = fields[1];
+    } else if (fields[2] == fields[5] && fields[5] == fields[8]) {
+        winner = fields[2];
+    } else if (fields[0] == fields[4] && fields[4] == fields[8]) {
+        winner = fields[0];
+    } else if (fields[2] == fields[4] && fields[4] == fields[6]) {
+        winner = fields[2];
+    }
+
+    if (winner) {
+        console.log('Gewonnen', winner);
+    }
 }
