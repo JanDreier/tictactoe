06ac1be4678481f5cb61a48b41747638ba11386f
Jan Dreier <Dreier-96@web.de>
2021-11-25 14:34:55 +0100

    Finished tictactoe

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let fields = [];
-
+let gameOver = false;
 let currentShape = ['cross'];
 
 function fillShape(id) {
-    if (!fields[id]) {
+    if (!fields[id] && !gameOver) {
 
 
         if (currentShape == 'cross') {
@@ -39,25 +39,55 @@ function draw() {
 function checkForWin() {
     let winner;
 
-    if (fields[0] == fields[1] && fields[1] == fields[2]) {
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
-    } else if (fields[3] == fields[4] && fields[4] == fields[5]) {
+        document.getElementById('line-1').style.transform = 'scaleX(1)';
+    } else if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
-    } else if (fields[6] == fields[7] && fields[7] == fields[8]) {
+        document.getElementById('line-2').style.transform = 'scaleX(1)';
+    } else if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
-    } else if (fields[0] == fields[3] && fields[3] == fields[6]) {
+        document.getElementById('line-3').style.transform = 'scaleX(1)';
+    } else if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
-    } else if (fields[1] == fields[4] && fields[4] == fields[7]) {
+        document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1)';
+    } else if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
-    } else if (fields[2] == fields[5] && fields[5] == fields[8]) {
+        document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
+    } else if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
-    } else if (fields[0] == fields[4] && fields[4] == fields[8]) {
+        document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)';
+    } else if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
-    } else if (fields[2] == fields[4] && fields[4] == fields[6]) {
+        document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1.2)';
+    } else if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1.2)';
     }
 
     if (winner) {
         console.log('Gewonnen', winner);
+        gameOver = true;
+        setTimeout(() => {
+            document.getElementById('replay-button').classList.remove('d-none');
+        }, 1000)
     }
+}
+
+function replay() {
+    gameOver = false;
+    fields = [];
+    document.getElementById('replay-button').classList.add('d-none')
+    for (let i = 1; i < 9; i++) {
+        document.getElementById('line-' + i).classList.add('d-none');
+
+    }
+
+    for (let i = 0; i < 9; i++) {
+        document.getElementById('cross-' + i).classList.add('d-none');
+        document.getElementById('circle-' + i).classList.add('d-none');
+    }
+    currentShape = ['cross'];
+    document.getElementById('player-1').classList.remove('player-inactive');
+    document.getElementById('player-2').classList.add('player-inactive');
 }
